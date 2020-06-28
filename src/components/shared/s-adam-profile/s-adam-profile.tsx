23f55e0b61ed756e99f63dcec6f7e10430d1f4f1
Saqhan <saqhan1@mail.ru>
2020-06-28 15:37:47 +0300

    fix m-b 70 for user profile

--- src/components/shared/s-adam-profile/s-adam-profile.tsx
+++ src/components/shared/s-adam-profile/s-adam-profile.tsx
@@ -25,68 +25,72 @@ export class SAdamProfile implements ComponentInterface {
       <section class="profile">
         <div class={this.getClassForHost()}>
           <div class="links-wrapper">
-            <a onClick={() => this.clickToLink.emit({ place: "showPersonalDialog" })}>
+            <a
+              onClick={() =>
+                this.clickToLink.emit({ place: "showPersonalDialog" })
+              }
+            >
               {" "}
               <i class="fas fa-arrow-left"></i>
             </a>
           </div>
-        </div>
-        <div class="about-user">
-          <div
-            class="profile-user-photo border-radius"
-            style={{
-              backgroundImage:
-                "url(https://via.placeholder.com/200x200?text=Text)",
-            }}
-          ></div>
-          <div class="user-name">Tim Ostin</div>
-          <div class="user-mail">timostin@gmail.com</div>
-          <div class="user-contact">
-            <i class="fas fa-phone-alt hover-link"></i>
-            <i class="fas fa-video hover-link"></i>
-            <i class="fas fa-envelope hover-link"></i>
-          </div>
-        </div>
-        <div class="user-social">
-          <div class="user-social-title">Terhubung</div>
-          <div class="user-social-block">
-            <div class="user-social-link">
-              <i class="fab fa-instagram hover-link"></i>
-              <span class="user-social-name">TimOstin</span>
-            </div>
-            <div class="user-social-link">
-              <i class="fab fa-twitter hover-link"></i>
-              <span class="user-social-name">Tim Ostin</span>
-            </div>
-            <div class="user-social-link">
-              <i class="fab fa-facebook-f р"></i>
-              <span class="user-social-name">Tim Ostin</span>
+          <div class="about-user">
+            <div
+              class="profile-user-photo border-radius"
+              style={{
+                backgroundImage:
+                  "url(https://via.placeholder.com/200x200?text=Text)",
+              }}
+            ></div>
+            <div class="user-name">Tim Ostin</div>
+            <div class="user-mail">timostin@gmail.com</div>
+            <div class="user-contact">
+              <i class="fas fa-phone-alt hover-link"></i>
+              <i class="fas fa-video hover-link"></i>
+              <i class="fas fa-envelope hover-link"></i>
             </div>
           </div>
-        </div>
-        <div class="media">
-          <div class="media-header">
-            <div class="media-title">Media</div>
-            <div class="media-descr">Lihat lebin banyak</div>
+          <div class="user-social">
+            <div class="user-social-title">Terhubung</div>
+            <div class="user-social-block">
+              <div class="user-social-link">
+                <i class="fab fa-instagram hover-link"></i>
+                <span class="user-social-name">TimOstin</span>
+              </div>
+              <div class="user-social-link">
+                <i class="fab fa-twitter hover-link"></i>
+                <span class="user-social-name">Tim Ostin</span>
+              </div>
+              <div class="user-social-link">
+                <i class="fab fa-facebook-f р"></i>
+                <span class="user-social-name">Tim Ostin</span>
+              </div>
+            </div>
           </div>
-          <div class="media-block">
-            <div class="media-item">
-              <div class="mediafile pdf">
-                <span>PDF</span>
-              </div>
-              <span class="mediafile-desc">Buku Hueanghong.pdf</span>
+          <div class="media">
+            <div class="media-header">
+              <div class="media-title">Media</div>
+              <div class="media-descr">Lihat lebin banyak</div>
             </div>
-            <div class="media-item">
-              <div class="mediafile zip">
-                <span>ZIP</span>
+            <div class="media-block">
+              <div class="media-item">
+                <div class="mediafile pdf">
+                  <span>PDF</span>
+                </div>
+                <span class="mediafile-desc">Buku Hueanghong.pdf</span>
               </div>
-              <span class="mediafile-desc">File jungjang.zip</span>
-            </div>
-            <div class="media-item">
-              <div class="mediafile doc">
-                <span>DOC</span>
+              <div class="media-item">
+                <div class="mediafile zip">
+                  <span>ZIP</span>
+                </div>
+                <span class="mediafile-desc">File jungjang.zip</span>
               </div>
-              <span class="mediafile-desc">Laporan keuangan.ppt</span>
+              <div class="media-item">
+                <div class="mediafile doc">
+                  <span>DOC</span>
+                </div>
+                <span class="mediafile-desc">Laporan keuangan.ppt</span>
+              </div>
             </div>
           </div>
         </div>
