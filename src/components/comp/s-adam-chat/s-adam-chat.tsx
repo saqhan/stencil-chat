c0041b1a2a376eb50ff179a04bec02ab866650ca
freeside <almalib@bk.ru>
2020-07-01 17:46:26 +0300

    add new styles

--- src/components/comp/s-adam-chat/s-adam-chat.tsx
+++ src/components/comp/s-adam-chat/s-adam-chat.tsx
@@ -1,18 +1,16 @@
-import {Component, ComponentInterface, h, State} from '@stencil/core';
-import {logo, navItems, dialogs, MessageMock} from "../../../utils/mock";
-
+import { Component, ComponentInterface, h, State } from "@stencil/core";
+import { logo, navItems, dialogs, MessageMock } from "../../../utils/mock";
 
 @Component({
-  tag: 's-adam-chat',
-  styleUrl: 's-adam-chat.css',
+  tag: "s-adam-chat",
+  styleUrl: "s-adam-chat.css",
   shadow: false,
 })
-export class SAdamChat implements ComponentInterface  {
-
+export class SAdamChat implements ComponentInterface {
   /**
    *Стейт для переключения на личный чат
    */
-  @State() dialogVisible = 'noChat';
+  @State() dialogVisible = "noChat";
 
   /**
    * Стейт для показа профиля пользователя
@@ -23,74 +21,87 @@ export class SAdamChat implements ComponentInterface  {
    * Метод для переключения на личный чат
    * @param detail
    */
-  public toggleChat({detail}) {
-    if(detail.place === 'showPersonalDialog') {
-      this.dialogVisible = 'showChat';
+  public toggleChat({ detail }) {
+    if (detail.place === "showPersonalDialog") {
+      this.dialogVisible = "showChat";
     }
   }
 
-  public toggleProfile({detail}) {
-    if (detail.place === 'userName') {
+  public toggleProfile({ detail }) {
+    if (detail.place === "userName") {
       this.profileVisible = !this.profileVisible;
     }
   }
 
   render() {
-    return <main>
-      <div class="container">
-        <div class="row">
-          <div class="col-1">
-            <s-adam-navigate logo={logo} navItems={navItems}></s-adam-navigate>
+    return (
+      <div class="main-wrapper">
+        <main>
+          <div class="container">
+            <div class="row">
+              <div class="col-1 white-col">
+                <s-adam-navigate
+                  logo={logo}
+                  navItems={navItems}
+                ></s-adam-navigate>
+              </div>
+              <div class="col-3">
+                <s-adam-contacts
+                  onClickToLink={({ detail }) => {
+                    this.toggleChat({ detail });
+                  }}
+                  dialogs={dialogs}
+                ></s-adam-contacts>
+              </div>
+              <div class="col white-col">
+                <section class="chat">
+                  {this.showChat(this.dialogVisible)}
+                </section>
+              </div>
+              {this.showProfile(this.profileVisible)}
+            </div>
           </div>
-          <div class="col-3">
-            <s-adam-contacts onClickToLink={({detail}) => {this.toggleChat({detail})}} dialogs={dialogs}>
-            </s-adam-contacts>
-          </div>
-          <div class="col">
-            <section class="chat">
-              {this.showChat(this.dialogVisible)}
-            </section>
-          </div>
-          {this.showProfile(this.profileVisible)}
-        </div>
+        </main>
       </div>
-    </main>
+    );
   }
 
   /**
    * Метод, который выводит необходимый чат
    * @param content
    */
-  public showChat (content){
+  public showChat(content) {
     switch (content) {
-      case 'showChat':
-        return <s-adam-direct
-          onClickToLink={({detail}) => this.clickToLink({detail})}
-          onClickOnUsername={(item) => {this.toggleProfile(item)}}
-          message={MessageMock}
-        >
-        </s-adam-direct>;
-      case 'noChat':
+      case "showChat":
+        return (
+          <s-adam-direct
+            onClickToLink={({ detail }) => this.clickToLink({ detail })}
+            onClickOnUsername={(item) => {
+              this.toggleProfile(item);
+            }}
+            message={MessageMock}
+          ></s-adam-direct>
+        );
+      case "noChat":
         return <s-adam-no-chat></s-adam-no-chat>;
     }
   }
 
-  public showProfile (item) {
-    if(item === true) {
+  public showProfile(item) {
+    if (item === true) {
       return (
         <div class="col-3">
           <s-adam-profile></s-adam-profile>
         </div>
-      )
-    }
-    else {
-      return '';
+      );
+    } else {
+      return "";
     }
   }
-/**
-* click to Link
-* */
-public clickToLink({ detail }) {
-  console.log(detail);
-}
+  /**
+   * click to Link
+   * */
+  public clickToLink({ detail }) {
+    console.log(detail);
+  }
 }
