7494069d70da4541cbd32a08fa0a285aae113f2b
freeside <almalib@bk.ru>
2020-07-08 18:02:48 +0300

    added timer

--- src/components/shared/personal-footer/res/view/footer-audio/footer-audio.tsx
+++ src/components/shared/personal-footer/res/view/footer-audio/footer-audio.tsx
@@ -1,8 +1,5 @@
-import {
-  Component,
-  ComponentInterface,
-  h,
-} from "@stencil/core";
+import { Component, ComponentInterface, h } from "@stencil/core";
+import dayjs from "dayjs";
 
 @Component({
   tag: "footer-audio",
@@ -16,7 +13,7 @@ export class FooterAudio implements ComponentInterface {
         <div class="footer-wrapper">
           <div class="record-dot"></div>
           <div class="timer">
-            <span id="minutes">00</span>:<span id="seconds" >00</span>
+            <span id="selector"></span>
           </div>
           <div class="input-wrapper">
             <form>
@@ -28,10 +25,7 @@ export class FooterAudio implements ComponentInterface {
               />
             </form>
           </div>
-          <div
-            class="audio"
-            id="audio"
-          >
+          <div class="audio" id="audio">
             <i class="fas fa-microphone"></i>
           </div>
           <div class="recording-btn"></div>
@@ -40,4 +34,59 @@ export class FooterAudio implements ComponentInterface {
     );
   }
 
+  componentDidLoad() {
+    const stopWatch = document.getElementById("selector");
+    const timer = new Timer();
+    console.log("didload");
+    timer.start((time: string, startedTime: Date) => {
+      {
+        (stopWatch.innerText = time), startedTime;
+      }
+    });
+  }
+}
+
+class Timer {
+  /**
+   * started time
+   */
+  private startedTime: Date;
+
+  /**
+   * Current work interval id
+   */
+  private intervalId;
+
+  /**
+   * Passed callback func
+   */
+  private callbackFunction: (time: string, startedTime: Date) => void;
+
+  /**
+   * Start timer
+   */
+  public start(cb: (time: string, startedTime: Date) => void) {
+    this.startedTime = new Date();
+    this.callbackFunction = cb;
+    this.intervalId = setInterval(() => {
+      const timeInMinutes = dayjs(
+        Date.now() - this.startedTime.getTime()
+      ).format("mm:ss");
+      cb(timeInMinutes, this.startedTime);
+    }, 0);
+  }
+
+  /**
+   * Reset timer
+   */
+  public reset() {
+    this.start(this.callbackFunction);
+  }
+
+  /**
+   * Stop timer
+   */
+  public stop() {
+    clearInterval(this.intervalId);
+  }
 }
