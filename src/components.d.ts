4cff3821cc165a93aa246fbe5ad3b29ff98c9059
Зураб Магомадов <zurabmagomadov@MacBook-Pro.local>
2020-07-11 17:53:45 +0300

    last

--- src/components.d.ts
+++ src/components.d.ts
@@ -5,9 +5,7 @@
  * It contains typing information for all components that exist in this project.
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
-import { ChatContactInterface, } from "./index";
-import { ChatCategoryInterface, ChatDialogInterface, ChatMessage, } from "./components/shared/interface/common.interface";
-import { ChatMessage as ChatMessage1, } from ".";
+import { ChatContactInterface, ChatMessage, ChatCategoryInterface, ChatDialogInterface} from "./index";
 import { logo, navItems, } from "./components/comp/s-adam-navigate/res/interface/common.interface";
 export namespace Components {
     interface BtnWrapper {
